a7d4604ec6d892f150b52bcfacafae2ca7c0c181
0xd0m7 <0xd0m7@gmail.com>
2023-11-29 17:30:46 +0100

    Update poc.py

--- poc.py
+++ poc.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route("/ca/inici/index.html")
 def poc():
-  message = '<img/scr=alert("POC by 0xd0m7")>'
+  message = '<img/src=alert("POC by 0xd0m7")>'
   return message
