9bfd1293cd54f5f91d28b81d1ce62579d628f212
0xd0m7 <0xd0m7@gmail.com>
2023-11-29 17:35:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/ca/inici/index.html")
-def poc():
-  message = '<img/scr=alert("POC by 0xd0m7")>'
-  return message
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+  content = f"""<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<script>alert("POC by 0xd0m7")</script>
+</body></html>
+"""
+  return content
